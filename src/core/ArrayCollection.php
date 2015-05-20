32bc338f2259b44c2d49e53e662e3c769bbdec0b
Xavier Ramirez <xramirez@latamautos.com>
2015-05-20 14:22:16 -0500

    Se agrego una nueva clase de array collecion
    Se agrego un metodo para buscar por id

---
+++ src/core/ArrayCollection.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xavier
+ * Date: 5/20/15
+ * Time: 2:12 PM
+ */
+
+namespace Latamautos\MicroserviceGateway\core;
+
+
+class ArrayCollection extends \Doctrine\Common\Collections\ArrayCollection {
+
+
+	public function findById($id) {
+		$resultArray = $this->filter(function ($entry) use ($id) {
+			return is_object($entry) && method_exists($entry, "getId") && $entry->getId() == $id;
+		});
+		if (!empty($resultArray) && is_array($resultArray)) {
+			return $resultArray[0];
+		}
+		return null;
+	}
+}
